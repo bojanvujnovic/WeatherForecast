025d385a398679fa0406ec5ed3f54d169249d8b7
Bojan Vujnovic <bojan.vujn@yahoo.com>
2017-02-22 12:04:37 +0100

    TableView

--- WeatherForecast/WeatherVC.swift
+++ WeatherForecast/WeatherVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 
 
-class WeatherVC: UIViewController {
+class WeatherVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var currentTempLabel: UILabel!
@@ -23,10 +23,22 @@ class WeatherVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
     }
 
-   
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "weatherCell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
+    }
 
 }
 
